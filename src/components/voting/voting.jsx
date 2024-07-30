e375a3b3308e3c286fcc7d077b916873ec5adc1c
Nathan Freeman <nathanlfreeman@gmail.com>
2024-07-30 16:08:05 -0400

    Update voting

--- src/components/voting/voting.jsx
+++ src/components/voting/voting.jsx
@@ -1,10 +1,11 @@
+import './voting.css'
 const Voting = () => {
     return (
       <section id="voting">
-        <h2 className="section-title">Voting in the midterm election</h2>
-        <h4 class="special-note">
+        <h2 className="section-title">Voting in the general election</h2>
+        <h3 class="special-note center">
           Exercise your most vital civic power! Vote!
-        </h4>
+        </h3>
         <h4>
           We leave late enough this year that you likely have enough time to
           pick up an absentee ballot! Visit{" "}
@@ -13,6 +14,7 @@ const Voting = () => {
           </a>{" "}
           to learn how to vote absentee this year.
         </h4>
+        <p>In previous years, I have been able to show up in person at my county's Board of Elections to fill out my absentee ballot. But you can usually request one to be delivered via mail that you can fill out and return via mail, if you prefer that method.</p>
       </section>
     );
 }
