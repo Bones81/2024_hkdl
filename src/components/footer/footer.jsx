d28bf954a9dc456f297b7919352333386fe04545
Nathan Freeman <nathanlfreeman@gmail.com>
2024-08-01 04:01:07 -0400

    Update header and footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,34 +2,81 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <footer id='footer'>
-            <h2 id="burger-info">
-                Use the menu in the upper right corner
-                to visit different sections of the site! &#8599;
-            </h2>
-            <div className="footer-img-grid">
-                <img src="assets/AllCarolers2019.jpg" alt="2019 carolers" />
-                <img src="assets/ChelseaCarl.jpg" alt="Chelsea with Carl"/>
-                <img src="assets/AlenaCarl.jpg" alt='Alena with Carl' />
-                <img src="assets/randomfriends.jpg" alt='random friends'/>
-                <img src="assets/2016 Xmas team photo.jpg" alt='2016 Christmas team'/>
-                <img src="assets/Vitality Bird's Nests and Chicken Essence.jpg" alt='funny sign in drug store'/>
-                <img src="assets/Cute HK Kids.jpg" alt='cute Hong Kong kids'/>
-                <img src="assets/HK Sunset Skyline.jpg" alt='HK sunset skyline'/>
-                <img src="assets/TL_Quartet_2022.jpeg" alt='2022 carolers performing the Tree Lighting show'/>
-                <img src="assets/Carolers_Silly_2022.jpeg" alt='2022 carolers acting silly'/>
-                <img src="assets/HKDL_Castle_2022.jpeg" alt='HKDL Castle of Magical Dreams at night, 2022'/>
-                <img src="assets/HKDL_Carolers_LKF_2019.jpeg" alt='2019 Carolers living it up in Lan Kwai Fong'/>
-                <img src="assets/Max_HKDL_2022.jpeg" alt='Max performing at HKDL, 2022'/>
-                <img src="assets/HKDL_Carolers_2022_LastLooks.jpeg" alt='Hanging out one last time at the IFC rooftop terrace, 2022'/>
-            </div>
-            
-            
-            {/* <h3>Only because it was requested, here are the Disney <em>acappella</em> parodies I put together in the first months of the pandemic:</h3> 
+      <footer id="footer">
+        <h2 id="burger-info">
+          Use the menu in the upper right corner to visit different sections of
+          the site! &#8599;
+        </h2>
+        <h3 className='centered'>An incredible experience awaits!</h3>
+        <div className="footer-img-grid">
+          <img src="assets/AllCarolers2019.jpg" alt="2019 carolers" />
+          <img
+            src="https://i.imgur.com/h20JNTc.jpeg"
+            alt="Carolers after Christmas Cabaret 2023"
+          />
+          <img
+            src="https://i.imgur.com/pEhqzFT.jpeg"
+            alt="2023 Carolers pic group shot in front of the castle"
+          />
+          <img src="assets/randomfriends.jpg" alt="random friends" />
+          <img
+            src="assets/2016 Xmas team photo.jpg"
+            alt="2016 Christmas team"
+          />
+          <img
+            src="assets/Vitality Bird's Nests and Chicken Essence.jpg"
+            alt="funny sign in drug store"
+          />
+          <img
+            src="https://i.imgur.com/f71NtU6.jpeg"
+            alt="2023 Group shot with Nelson"
+          />
+          <img src="assets/Cute HK Kids.jpg" alt="cute Hong Kong kids" />
+          <img src="assets/HK Sunset Skyline.jpg" alt="HK sunset skyline" />
+          <img
+            src="https://i.imgur.com/zXJ6rMK.jpeg"
+            alt="2023 Fantasyland Carolers"
+          />
+          <img
+            src="assets/TL_Quartet_2022.jpeg"
+            alt="2022 carolers performing the Tree Lighting show"
+          />
+          <img src="https://i.imgur.com/FecbJBi.jpeg" alt="HK skyline from the Peak" />
+          <img
+            src="https://i.imgur.com/M7wl1WK.jpeg"
+            alt="Animated 2023 carolers"
+          />
+          <img
+            src="assets/Carolers_Silly_2022.jpeg"
+            alt="2022 carolers acting silly"
+          />
+          <img
+            src="assets/HKDL_Castle_2022.jpeg"
+            alt="HKDL Castle of Magical Dreams at night, 2022"
+          />
+          <img
+            src="assets/HKDL_Carolers_LKF_2019.jpeg"
+            alt="2019 Carolers living it up in Lan Kwai Fong"
+          />
+          <img
+            src="assets/Max_HKDL_2022.jpeg"
+            alt="Max performing at HKDL, 2022"
+          />
+          <img
+            src="https://i.imgur.com/21KbOaw.jpeg"
+            alt="2023 Carolers at DLH"
+          />
+          <img
+            src="assets/HKDL_Carolers_2022_LastLooks.jpeg"
+            alt="Hanging out one last time at the IFC rooftop terrace, 2022"
+          />
+        </div>
+
+        {/* <h3>Only because it was requested, here are the Disney <em>acappella</em> parodies I put together in the first months of the pandemic:</h3> 
             <iframe width="560" height="315" src="https://www.youtube.com/embed/a_XCrClEFvE" frameorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title='Disney parody video Social Distance'></iframe>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/ljtlm2LYiLo" frameorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title='Disney parody video Best If You Stay Home'></iframe> */}
-        </footer>
-    )
+      </footer>
+    );
 }
 
 export default Footer
