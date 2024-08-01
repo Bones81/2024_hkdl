d4d61cf59ba240866661c2dc5dface6eb2f361d1
Nathan Freeman <nathanlfreeman@gmail.com>
2024-07-31 21:05:59 -0400

    Update health

--- src/components/health_care/health_care.jsx
+++ src/components/health_care/health_care.jsx
@@ -1,34 +1,60 @@
 const Health = () => {
     return (
       <section id="health">
-        <h2 className="section-title">Health Care and Insurance in HK</h2>
+        <h2 className="section-title">Health Care, Health Maintenance, and Insurance in HK</h2>
+        <h3>Our Insurance in HK</h3>
         <h4 class="special-note">
-          Our Disney-provided health insurance is supposed to be pretty high
+          Our HK Disney-provided AIA health insurance is pretty high
           quality. I know one previous
           caroler had to get a root canal done in HK. And another had multiple
-          outpatient visits to local hospitals for rough illnesses during a
-          contract.
+          outpatient visits to local hospitals for rough illnesses in 2019. 
         </h4>
         <p>
-          In 2023, some pretty rough illnesses spread through the dancers and vocalists at HKDL. Unfortunately, despite taking many precautions, pretty much all of us dealt with some version of the illness, and a few of us did have to take sick days off of work.
-        </p>
-        <p>A doctor's note is required to clear your sick days. As a result, even though you may feel pretty awful, you need to visit a local clinic with a doctor who takes our insurance. Fortunately there were a couple of clinics about a 10-minute walk from our HPRC housing. I will try to post their info here so that we can be aware of them should we need them this year.</p>
-        <p>The doctor's visits were ultimately very efficient, with the doctor asking a few questions to determine the nature of the illness, followed by issuing the doctor's note and several different meds targeting any and all symptoms. With our insurance, there was HKD$0 for all services.</p>
-        <p>
-          As part of our first couple of weeks at Disney, we will eventually
+          During our first couple of weeks at Disney, we will
           receive a packet with our health insurance benefits and information.
-          We will receive a health insurance card at some point too. And we are
-          expected to turn it in at the end of the contract. I'm fairly certain
+          We will receive a health insurance card from our stage managers too. We are
+          expected to return it at the end of the contract. I'm fairly certain
           our insurance covers full medical and dental services.
         </p>
+        <p>If you can make the time, maybe take advantage of our health insurance to get a needed doctor's check-up or a dental cleaning!</p>
         <p>
-          One form we will receive and be advised to fill out is a Beneficiary
+          One form we will receive is a Beneficiary
           form. It's basically a form that identifies who gets your benefits if
           you die under our insurance policy. There is a drop bin for the form
           by the Disney mailroom in 602 (the main administrative building for
           HKDL). But it is not a critical form, given the high unlikelihood of
           us dying. I often lose track of it before the end of the contract.
         </p>
+        <h4>Recent illness experience</h4>
+        <p>
+          In 2023, some pretty rough illnesses spread through the dancers and vocalists at HKDL. Unfortunately, despite taking many precautions, pretty much all of us dealt with some version of the illness, and a few of us did have to take sick days off of work.
+        </p>
+        <p>Fortunately, we do accumulate 2 days of sick leave after our first month of work, so if you get sick in December, you should be able to qualify for up to 2 days of paid sick leave.</p>
+        <img src="https://i.imgur.com/Gqh8ioi.jpeg" alt="doctor's notes from 2023" className="list-pic"/>
+        <p className="img-caption">Examples of doctor's notes you will need to submit to qualify for paid sick leave</p>
+        <p>A doctor's note is required to clear your sick days. As a result, even though you may feel pretty awful, you need to visit a local clinic with a doctor who takes our insurance. Fortunately there were a couple of clinics that take our AIA insurance about a 10-minute walk from our HPRC housing. Here is the <a href="https://maps.app.goo.gl/bgmeSouSdjBZbtLD9?g_st=com.google.maps.preview.copy" target="_blank">Google Maps link to the Grace Medical Clinic</a> that I used in 2023.</p>
+        <img src="https://i.imgur.com/r5MxpLo.jpeg" alt="meds from HK doc visit" className="list-pic"/>
+        <p className="img-caption">Some of the meds I received after a HK doctor's visit in 2023 for a respiratory illness.</p>
+        <p>The doctor's visits were ultimately very efficient, with the doctor asking a few questions to determine the nature of the illness, followed by issuing the doctor's note and several different meds targeting any and all symptoms. With our insurance, the cost was HKD$0 for all services.</p>
+        <h3>Stay Healthy!</h3>
+        <h4>Self Care/Management</h4>
+        <p>We all have a LOT of requirements and goals for our time in HK. You will have 9-hour work days plus 1-2 hours of total commute on work days. Our workdays are often from 11am - 8pm, leaving little time before or after work to handle other needs like buying groceries, making dinner, socializing, talking to people back home, going to the gym, etc. And then of course, on your days off, most of you will hopefully want to SEE ALL THAT HONG KONG HAS TO OFFER!!</p>
+        <p>The point is that it is easy to get run down, worn out, or otherwise compromised by the intensity of the schedule. Inevitably, during every contract, people will realize they just need to take it easy for at least a day, even though others may have big plans. I recommend honoring that instinct when it pops up. I have seen what happens to myself and others when we go too hard. People get sick! And our health is too important, not just for our jobs but for the joy of the whole experience.</p>
+        <p>A somewhat-related occurrence that I know has occurred for myself and others in the past are moments of being depressed or overwhelmed, especially for first-timers. Everyone's experience will be different, of course, but just know, if any kind of depression hits you, that you're not alone in that experience and that your caroler friends are here to support you should you want it. 🤗🩷</p>
+        <h4>Helpful products</h4>
+        <p>Singers often have many helpful tools to maintain their health and voices and stay in vocal shape. And we are no exception. Here are some of ours:</p>
+        <ul>
+          <li>Nebulizers/Steamers/Vocal Misters: Be careful to use either battery-operated devices or dual-voltage devices so that they aren't fried by the 220V HK outlets.</li>
+          <li>Nin Jiom Pei Pa Koa: This honey loquat syrup with Chinese herbs has been a staple throat soothing nectar that can be used preventatively or when your throat is sore or irritated. It can be taken straight or made into a tea with hot water. Literally it's first indication is that it's for vocal performers!
+            <img src="https://i.imgur.com/o22VhKz.jpeg" alt="Nin Jiom Pei Pa Koa syrup" className="list-pic"/>
+            <p className="img-caption">Magical Stuff. Get a bottle at any 7-11 or drug store.</p>
+          </li>
+          <li>MODERATION: Essentially, this is a repeat of the Self Care/Management section above. Be aware and careful about overusing your voice outside of work. If you go to Ned Kelly's Last Stand Jazz Pub, for example (and you absolutely will/should), it will be loud, and you will need to raise your voice just to have conversations. Just know thyself, and be careful. It can be a very fine line between maximizing your joy in Hong Kong and overexerting yourself in a way that leads to illness or reduced vocal capacity. You're all vocal pros, though. I'm sure you get it.</li>
+          <li>SLEEP: Get enough! It can be hard at times in HK. Don't neglect it. Getting enough will enhance the rest of your experience!</li>
+          <li>Other preventative care: Whatever works for you, be it yoga, essential oils treatments, baths, long showers, tea, whatever! Do what works for you!</li>
+        </ul>
+        <h4>Health Conditions / Medication</h4>
+        <p>If you have any particular health conditions that require medication, be aware that some medications that are legal in the US are not legal in Hong Kong and could lead to jail time (!) if you were found to be "smuggling" them into the country. We discovered this last year, as two of our folks needed to apply for special exemptions to bring in restricted medications. However, their experiences in getting those exemptions were very different, despite trying to get approved to bring the same types of medicines. Let us know if you think you need any help with your medication situation. One place you may check for additional information is the <a href="https://hk.usconsulate.gov/" target="_blank">US Consulate in Hong Kong</a>, as it's their job to protect travelers abroad, and they may well know the rules better than any of us.</p>
       </section>
     );
 }
